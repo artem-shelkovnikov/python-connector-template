74c0f4b9cbe9a2a861865535b02d8007df0e7fdf
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2022-01-10 18:23:12 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,25 @@
+PYTHON = python3
+PIP = pip3
+
+.DEFAULT_GOAL = help
+
+help:
+	@echo "make venv_init - set up and activate venv for the project"
+	@echo "make setup - set up the project locally"
+	@echo "make test - run the tests for the project"
+	@echo "make clean - remove venv directory from the project"
+
+venv_init:
+	${PYTHON} -m venv venv
+	. venv/bin/activate
+
+setup:
+	${PIP} install -r requirements.txt
+	${PIP} install -r tests/requirements.txt
+	${PYTHON} setup.py install
+
+test:
+	${PYTHON} -m pytest
+
+clean:
+	rm -r venv
---
+++ setup.py
@@ -0,0 +1,56 @@
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 6):
+    raise ValueError("Requires Python 3.6 or superior")
+
+from python_connector_template import __version__  # NOQA
+
+install_requires = [
+    "elastic_enterprise_search",
+    "pyyaml",
+    "tika",
+    "ecs_logging"
+]
+
+description = ""
+
+for file_ in ("README", "CHANGELOG"):
+    with open("%s.rst" % file_) as f:
+        description += f.read() + "\n\n"
+
+
+classifiers = [
+    "Programming Language :: Python",
+    "License :: OSI Approved :: Apache Software License",
+    "Development Status :: 5 - Production/Stable",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+]
+
+
+setup(
+    name="python connector template",
+    version=__version__,
+    url="https://example.com",
+    packages=find_packages(),
+    long_description=description.strip(),
+    description=("This is a template python project for a Enterprise Search Connector."),
+    author="author",
+    author_email="email",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=classifiers,
+    install_requires=install_requires,
+    entry_points="""
+      [console_scripts]
+      bootstrap = python_connector_template.bootstrap:main
+      check_connectivity = python_connector_template.check_connectivity:main
+      full_sync = python_connector_template.full_sync:main
+      incremental_sync = python_connector_template.incremental_sync:main
+      deletions_sync = python_connector_template.deletion_sync:main
+      """,
+)
