f28cdd0981632c5a8dcdfb25f088ee7b7eab9e6a
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2022-01-13 15:02:06 +0100

    More recent changes

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 PYTHON = python3
 PIP = pip3
+VENV_DIRECTORY = venv
+PROJECT_DIRECTORY = python_connector_template
 
 .DEFAULT_GOAL = help
 
@@ -7,20 +9,32 @@ help:
 	@echo "make venv_init - set up and activate venv for the project"
 	@echo "make setup - set up the project locally"
 	@echo "make test - run the tests for the project"
+	@echo "make cover - check test coverage for the project"
+	@echo "make lint - run linter against the project"
 	@echo "make clean - remove venv directory from the project"
 
 venv_init:
-	${PYTHON} -m venv venv
+	${PYTHON} -m venv ${VENV_DIRECTORY}
 
 setup:
-	${PIP} install -r requirements.txt
-	${PIP} install -r tests/requirements.txt
-	${PIP} install .
+	${VENV_DIRECTORY}/bin/${PIP} install -r requirements.txt
+	${VENV_DIRECTORY}/bin/${PIP} install -r tests/requirements.txt
+
+install_locally:
+	${VENV_DIRECTORY}/bin/${PIP} install .
 
 test:
-	${PYTHON} -m pytest
+	${VENV_DIRECTORY}/bin/${PYTHON} -m pytest
+
+cover:
+	${VENV_DIRECTORY}/bin/pytest --cov ${PROJECT_DIRECTORY} --cov-fail-under=80 tests
+
+lint:
+	flake8 ${PROJECT_DIRECTORY}
 
 clean:
 	rm -rf venv
 	rm -rf build
 	rm -rf *.egg-info
+	rm -rf .pytest_cache
+	rm .coverage
--- python_connector_template/cmd.py
+++ python_connector_template/cmd.py
@@ -3,18 +3,74 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
+"""cmd module contains entry points to the connector.
+
+Each method provides a way to make an interaction
+between Elastic Enterprise Search and remote system.
+
+For example, full_sync provides a command that will attempt to sync
+all data from remote system to Elastic Enterprise Search.
+
+See each individual command for the description.
+"""
+
+import sys
+
 
 def bootstrap():
+    """bootstrap command will attempt to create a Content Source in EES instance.
+
+    If successful, it will create a Content Source for this connector in
+    Elastic  Enterprise Search instance and return its id that can be put
+    into workplace_search.source_id config option.
+    """
+
     print('Running Bootstrap')
 
+
 def check_connectivity():
+    """This command will check connection to remote systems.
+
+    It will attempt to make a call to Elastic Enterprise Search and
+    third-party system that this connector will attempt to sync
+    data from.
+    """
     print('Running Check Connectivity')
 
+
 def full_sync():
+    """This command will attempt to sync all data from remote system.
+
+    It will attempt to fetch all data from the beginning of time and
+    send this data to Elastic Enterprise Search instance.
+    """
     print('Running Full Sync')
 
+
 def incremental_sync():
+    """This command will attempt to sync latest data from remote system.
+
+    It will attempt to fetch latest data from the time last sync
+    happened and send this data to Elastic Enterprise Search instance.
+    """
     print('Running Incremental Sync')
 
+
 def deletion_sync():
+    """This command will attempt to sync deleted data from remote system.
+
+    It will attempt to fetch information about which records were
+    deleted from third-party system recently and attempt to delete
+    these records from Elastic Enterprise Search instance.
+    """
     print('Running Deletion Sync')
+
+def permission_sync():
+    """This command will attempt to sync user permissions from remote system.
+
+    It will attempt to fetch the permissions of the users of remote system
+    and upload it to Elastic Enterprise Search. This permissions will be
+    used to show to user only the documents that he's allowed to see.
+    """
+    print("Running Permission Sync")
+    sys.exit(0)
